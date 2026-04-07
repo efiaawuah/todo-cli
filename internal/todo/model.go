e1f6f3ad82d7c94d1a6b93547ff6d41fd4032a47
Efia Awuah <efiaawuah@outlook.com>
2026-04-07 16:23:34 -0700

    adds Load and Save functions to store

--- internal/todo/model.go
+++ internal/todo/model.go
@@ -1,7 +1,12 @@
 package todo
 
-type Todo struct {
+type Task struct {
 	ID          int    `json:"id"`
 	DESCRIPTION string `json:"description"`
 	DONE        bool   `json:"done"`
+}
+
+type Todos struct {
+	NEXT_ID int    `json:"next_id"`
+	TASKS   []Task `json:"tasks"`
 }
--- internal/todo/store.go
+++ internal/todo/store.go
@@ -1,1 +1,59 @@
 package todo
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+// creates a hidden .todocli folder for data storage in user's home directory
+func getFilePath() string {
+	home, _ := os.UserHomeDir()
+	dir := filepath.Join(home, ".todocli")
+	os.MkdirAll(dir, 0755)
+	return filepath.Join(dir, "todos.json")
+}
+
+// loads todos data from json file into a todos struct
+func Load() (Todos, error) {
+	path := getFilePath()
+	file, err := os.ReadFile(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return Todos{NEXT_ID: 0, TASKS: []Task{}}, nil
+		}
+		return Todos{NEXT_ID: 0, TASKS: []Task{}}, err
+	}
+	var todos Todos
+	err = json.Unmarshal(file, &todos)
+	if err != nil {
+		panic(err)
+	}
+	return todos, nil
+}
+
+// puts todos data into json file
+func Save(todos Todos) error {
+	path := getFilePath()
+	data, err := json.MarshalIndent(todos, "", "")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func Add(task string) error {
+	return nil
+}
+
+func MarkComplete(task_id int) error {
+	return nil
+}
+
+func ListTasks(done *bool) []Task {
+	return nil
+}
+
+func Delete(task_id int) error {
+	return nil
+}
