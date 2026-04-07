5d13a875fd3324a49680d3c47fda9c6467519cda
Efia Awuah <efiaawuah@outlook.com>
2026-04-07 14:46:30 -0700

    added markComplete command

--- cmd/markComplete.go
+++ cmd/markComplete.go
@@ -9,26 +9,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add [task]",
-	Short: "Add a task",
+// markCompleteCmd represents the markComplete command
+var markCompleteCmd = &cobra.Command{
+	Use:   "markComplete [task_id]",
+	Short: "Marks task [task_id] as complete",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		fmt.Println("markComplete called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(markCompleteCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// markCompleteCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// markCompleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
