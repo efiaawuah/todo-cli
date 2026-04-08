227867474a915718a3120e0703fdad21c3ab7026
Efia Awuah <efiaawuah@outlook.com>
2026-04-07 17:25:34 -0700

    list tasks

--- cmd/list.go
+++ cmd/list.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"my-cli/internal/todo"
 
 	"github.com/spf13/cobra"
 )
@@ -15,12 +16,39 @@ var listCmd = &cobra.Command{
 	Short: "list tasks in todo list",
 	Long:  `lists tasks in todo list. Flag -complete for all done tasks and -inconplete for all incomplete tasks`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+
+		complete, _ := cmd.Flags().GetBool("complete")
+		incomplete, _ := cmd.Flags().GetBool("incomplete")
+
+		var filter *bool
+
+		if complete {
+			t := true
+			filter = &t
+		} else if incomplete {
+			f := false
+			filter = &f
+		} else {
+			filter = nil // no filter
+		}
+
+		err, tasks := todo.ListTasks(filter)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		for _, task := range tasks {
+			fmt.Printf("%d: %s (done=%v)\n", task.ID, task.DESCRIPTION, task.DONE)
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolP("complete", "c", false, "show only completed tasks")
+	listCmd.Flags().BoolP("incomplete", "i", false, "show only incomplete tasks")
 
 	// Here you will define your flags and configuration settings.
 
